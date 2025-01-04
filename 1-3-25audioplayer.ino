a2e1342bce879485f80c0bd81346555599569601
Derek Lin <derekhongdalin@gmail.com>
2025-01-04 15:14:19 -0500

    esp32 audio playwer

---
+++ 1-3-25audioplayer.ino
@@ -0,0 +1,129 @@
+//**********************************************************************************************************
+//*    audioI2S-- I2S audiodecoder for ESP32,                                                              *
+//**********************************************************************************************************
+//
+// first release on 11/2018
+// Version 3  , Jul.02/2020
+//
+//
+// THE SOFTWARE IS PROVIDED "AS IS" FOR PRIVATE USE ONLY, IT IS NOT FOR COMMERCIAL USE IN WHOLE OR PART OR CONCEPT.
+// FOR PERSONAL USE IT IS SUPPLIED WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHOR
+// OR COPYRIGHT HOLDER BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE
+//
+
+#include "Arduino.h"
+#include "WiFiMulti.h"
+#include "Audio.h"
+#include "sd_read_write.h"
+#include "SD_MMC.h"
+
+// Digital I/O used 
+#define SD_MMC_CMD      38
+#define SD_MMC_CLK      39
+#define SD_MMC_D0       40
+#define I2S_DOUT      18
+#define I2S_BCLK      4
+#define I2S_LRC       6
+
+Audio audio;
+WiFiMulti wifiMulti;
+String ssid =     "Ssl2021";
+String password = "dereklindevinlinseanlintinawu";
+
+
+void setup() {
+// init card
+    Serial.begin(115200);
+    Serial.println("Hey Please End mee * V * OWOWOOWWOOWOW");
+    SD_MMC.setPins(SD_MMC_CLK, SD_MMC_CMD, SD_MMC_D0);
+
+// check card type
+    if (!SD_MMC.begin("/sdcard", true, true, SDMMC_FREQ_DEFAULT, 5)) {
+      Serial.println("Card Mount Failed");
+      return;
+    }
+    uint8_t cardType = SD_MMC.cardType();
+    if(cardType == CARD_NONE){
+        Serial.println("No SD_MMC card attached");
+        return;
+    }
+
+    Serial.print("SD_MMC Card Type: ");
+    if(cardType == CARD_MMC){
+        Serial.println("MMC");
+    } else if(cardType == CARD_SD){
+        Serial.println("SDSC");
+    } else if(cardType == CARD_SDHC){
+        Serial.println("SDHC");
+    } else {
+        Serial.println("UNKNOWN");
+    }
+
+    uint64_t cardSize = SD_MMC.cardSize() / (1024 * 1024);
+    Serial.printf("SD_MMC Card Size: %lluMB\n", cardSize);
+
+    listDir(SD_MMC, "/", 0);
+
+
+// setup wifi
+    WiFi.mode(WIFI_STA);
+    wifiMulti.addAP(ssid.c_str(), password.c_str());
+    wifiMulti.run();
+    if(WiFi.status() != WL_CONNECTED){
+        WiFi.disconnect(true);
+        wifiMulti.run();
+    }
+
+    
+    audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
+    audio.setVolume(12); // 0...21
+
+//    audio.connecttoFS(SD_MMC, "/320k_test.mp3");
+    audio.connecttoFS(SD_MMC, "test.wav");
+//    audio.connecttohost("http://www.wdr.de/wdrlive/media/einslive.m3u");
+//    audio.connecttohost("http://macslons-irish-pub-radio.com/media.asx");
+//    audio.connecttohost("http://mp3.ffh.de/radioffh/hqlivestream.aac"); //  128k aac
+//      audio.connecttohost("http://mp3.ffh.de/radioffh/hqlivestream.mp3"); //  128k mp3
+} 
+
+void loop()
+{
+    audio.loop();
+    if(Serial.available()){ // put streamURL in serial monitor
+        audio.stopSong();
+        String r=Serial.readString(); r.trim();
+        if(r.length()>5) audio.connecttohost(r.c_str());
+        log_i("free heap=%i", ESP.getFreeHeap());
+    }
+}
+
+// optional
+void audio_info(const char *info){
+    Serial.print("info        "); Serial.println(info);
+}
+void audio_id3data(const char *info){  //id3 metadata
+    Serial.print("id3data     ");Serial.println(info);
+}
+void audio_eof_mp3(const char *info){  //end of file
+    Serial.print("eof_mp3     ");Serial.println(info);
+}
+void audio_showstation(const char *info){
+    Serial.print("station     ");Serial.println(info);
+}
+void audio_showstreamtitle(const char *info){
+    Serial.print("streamtitle ");Serial.println(info);
+}
+void audio_bitrate(const char *info){
+    Serial.print("bitrate     ");Serial.println(info);
+}
+void audio_commercial(const char *info){  //duration in sec
+    Serial.print("commercial  ");Serial.println(info);
+}
+void audio_icyurl(const char *info){  //homepage
+    Serial.print("icyurl      ");Serial.println(info);
+}
+void audio_lasthost(const char *info){  //stream URL played
+    Serial.print("lasthost    ");Serial.println(info);
+}
